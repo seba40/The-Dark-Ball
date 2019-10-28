34611a2d9c1eccdbb27f67d5cf6d53bc25ef419c
AlexTataru <54209061+AlexTataru@users.noreply.github.com>
2019-10-28 15:36:42 +0200

    Add new jump mechanic
    Modified the PlayerMovement script in order for the player the jump multiple amount of times.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     private float moveInput; // detects left or right key when pressed
     private  Rigidbody2D ballRigidBody; // variable for rigid body (private to only access it from the script)
     private bool grounded; // ground variable changes from true or false when object enters or exits collision with the ground
+    public float maxJumps = 2; // variable for the maximum number of jumps allowed
+    public float remainingJumps = 2; // variable for the number of jumps remaning
     
     void Start()
     {
@@ -17,9 +19,12 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal"); // if I press right key moveInput = 1,left key = -1 
         ballRigidBody.velocity = new Vector2(moveInput * speed, ballRigidBody.velocity.y); // adds force to the ball movement in order for the player to move,doesn't affect y axis
-        if (Input.GetKeyDown(KeyCode.Space) && grounded) // checks if jumping key is pressed
+        if (grounded)
+            remainingJumps = maxJumps; // if the player is grounded the remaningJumps variable is equal to the maxJum
+        if (Input.GetKeyDown(KeyCode.Space) && remainingJumps > 0) // checks if jumping key is pressed
         {
             ballRigidBody.AddForce(Vector2.up * thrust * 200);
+            remainingJumps--; // -- decreases the amount of jumps so the player doesn't jump an infinite amount of times
         }
     }
 
