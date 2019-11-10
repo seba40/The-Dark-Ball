a2e8b212e57b84067ef500a5e9bac322c919cfd4
AlexTataru <54209061+AlexTataru@users.noreply.github.com>
2019-11-10 12:40:24 +0200

    Add Controller Script
    Added new Controller script to detect when the player hits a "Respawn" tagged object in order to respawn.
    Added new sprite
    Added new scene

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    private bool death = false;
+    
+    public void YouDied() 
+    {
+        if(death == false) // Checks if the player has died or not
+        {
+            death = true;
+            Debug.Log("You Died");
+            Invoke("Reload", 3); // calls Reload function after a certain amount of time
+        }
+    }
+    private void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reloads the active scenen
+    }
+}
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,6 @@ public class PlayerMovement : MonoBehaviour
     private  Rigidbody2D ballRigidBody; // variable for rigid body (private to only access it from the script)
     public float maxJumps; // variable for the maximum number of jumps allowed
     private float remainingJumps; // variable for the number of jumps remaning
-
     
     void Start()
     {
@@ -26,13 +25,21 @@ public class PlayerMovement : MonoBehaviour
             ballRigidBody.AddForce(Vector2.up * thrust * 200);
             remainingJumps--; // -- decreases the amount of jumps so the player doesn't jump an infinite amount of times
         }
+       /* if (ballRigidBody.position.y < -10f) // if the y position is < or = to -10 the death variable in gamemanager script becomes true and the player dies and the game restarts
+            FindObjectOfType<Controller>().YouDied(); */
     }
     private void OnCollisionEnter2D(Collision2D collision) // function to detect when object enters in collision with ground
     {
         if (collision.gameObject.tag == "ground")
         {
-            remainingJumps = maxJumps; // if the player is grounded the remaningJumps variable is equal to the maxJum
+            remainingJumps = maxJumps; // if the player is grounded the remaningJumps variable is equal to the maxJumps            
         }
+        if (collision.gameObject.tag == "Respawn")
+        {
+            speed = 0;
+            thrust = 0;
+            FindObjectOfType<Controller>().YouDied();
+        }      
     }     
     }
     
